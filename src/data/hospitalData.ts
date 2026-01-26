6fe9db2c2d655057ba1e99c18daeb2bfb826f310
pranaybawankar <bawankarpranay41@gmail.com>
2026-01-26 10:58:08 +0530

    Update hospitalData.ts

--- src/data/hospitalData.ts
+++ src/data/hospitalData.ts
@@ -18,7 +18,7 @@ export const hospitalData = {
   
   contact: {
     phone: "+91 8446721259",
-    whatsapp:  "+91 8446721259",
+    whatsapp:  "8446721259",
     email: "",
     address: "Arsh Complex, near Madhura medical,Awanti bai Square, Gondiamap",
     mapEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.755537609912!2d72.87765347520897!3d19.024547253903636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cf26f4972f25%3A0x5c5b9c5c8f5f5f5f!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1699999999999!5m2!1sen!2sin"
